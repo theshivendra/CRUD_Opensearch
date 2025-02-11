db9c2b6a0015bb45e30094c98c419c2d1a035d68
Shivendra Shukla <69752251+theshivendra@users.noreply.github.com>
2025-02-12 01:37:41 +0530

    Update App.jsx
    To integrate KeyCloack authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+
+import React, { useState, useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Keycloak from 'keycloak-js';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import UserManagement from './pages/UserManagement';
 
 const App = () => {
+
+    const isRun = useRef(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+
+        const keycloak = new Keycloak({
+            url: 'http://localhost:8180', // Keycloak server URL
+            realm: 'myrealm',             // Your realm name
+            clientId: 'myclient',         // Your client ID
+        });
+if(isRun.current) return;
+    isRun.current = true;
+    keycloak.init({onLoad: "login-required"}).then((res) => {setIsAuthenticated(res);});
+    }, []);
+    if (!isAuthenticated) {
+        return <h2>Loading...</h2>;
+    }
     return (
         <Router>
             <Navbar />
